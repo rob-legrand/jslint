1910967246664bffbe6fc0e9a893bd70fd34456a
Rob LeGrand <roblegrand98@gmail.com>
2026-02-11 17:22:24 -0600

    Create nolet option to disallow let declarations

--- jslint.js
+++ jslint.js
@@ -113,6 +113,7 @@
     margin, match, message, misplaced_a, misplaced_directive_a, missing_browser,
     missing_m, module, naked_block, name, names, nested_comment, new, node,
     nofor,
+    nolet,
     nolong,
     nolonghand,
     not_label_a, nr, nud, number_isNaN, ok, open, opening, option,
@@ -203,6 +204,7 @@ const allowed_option = {
         "TextEncoder", "URL", "URLSearchParams", "__dirname", "__filename"
     ],
     nofor: false,
+    nolet: false,
     nolong: false,
     nolonghand: false,
     this: false,
@@ -3277,12 +3279,27 @@ function do_var() {
 
     if (!is_const) {
         if (!option.vardec && the_statement.id === "var") {
-            warn(
-                "expected_a_b",
-                the_statement,
-                "let",
-                the_statement.id
-            );
+            if (option.nolet) {
+                warn("unexpected_a", the_statement);
+            } else {
+                warn(
+                    "expected_a_b",
+                    the_statement,
+                    "let",
+                    the_statement.id
+                );
+            }
+        } else if (option.nolet && the_statement.id === "let") {
+            if (!option.vardec) {
+                warn("unexpected_a", the_statement);
+            } else {
+                warn(
+                    "expected_a_b",
+                    the_statement,
+                    "var",
+                    the_statement.id
+                );
+            }
         } else if (var_mode === undefined) {
             var_mode = the_statement.id;
         } else if (the_statement.id !== var_mode) {
@@ -5095,7 +5112,7 @@ export default Object.freeze(function jslint(
     }
     return {
         directives,
-        edition: "2020-11-06-RHL013",
+        edition: "2020-11-06-RHL014",
         exports,
         froms,
         functions,
